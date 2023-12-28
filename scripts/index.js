a1fb7a3406f90a011d250b88ac2d1ffc93d88c00
Alexey Ershov <eaa04@bk.ru>
2023-12-28 21:06:28 +0300

    something done

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,6 @@
 
 // @todo: Вывести карточки на страницу
 
-
 const deleteCard = evt => {
   const actionTarget = evt.target.closest('.places__item');
   actionTarget.remove();
@@ -39,3 +38,109 @@ const createCard = (link, name, deleteCard) => {
 initialCards.forEach((card) => {
   cardsList.append(createCard(card.link, card.name, deleteCard));
 });
+
+/* searching popups */
+const editProfilePopup = document.querySelector('.popup_type_edit');
+const createNewCardPopup = document.querySelector('.popup_type_new-card');
+const imagePopup = document.querySelector('.popup_type_image');
+
+/* open popup function */
+const openPopup = evt => {
+  if (evt.target.classList.contains('profile__edit-button')) {
+    editProfilePopup.classList.add('popup_is-opened');
+  }
+  if (evt.target.classList.contains('profile__add-button')) {
+    createNewCardPopup.classList.add('popup_is-opened');
+  }
+  if (evt.target.classList.contains('card__image')) {
+    imagePopup.classList.add('popup_is-opened');
+  }
+};
+
+document.addEventListener('click', openPopup);
+
+/* close popup function */
+const closePopup = () => {
+    imagePopup.classList.remove('popup_is-opened');
+    createNewCardPopup.classList.remove('popup_is-opened');
+    editProfilePopup.classList.remove('popup_is-opened');
+}
+
+document.addEventListener('click', function(evt) {
+  if (evt.target.classList.contains('popup__close')) {
+    closePopup();
+  }
+});
+
+document.addEventListener('keypress', function(evt) {
+  if (evt.key === '1') {
+    closePopup();
+  }
+}); /* CHECK !!! */
+
+/* information in profile popup */
+/* searching profile form and profile form fields */
+const editProfileForm = document.forms['edit-profile'];
+
+const profileNameField = editProfileForm.name;
+const profileDescriptionField = editProfileForm.description;
+
+/* searching name and description values */
+const profileName = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+
+/* add name and description values to edit profile form */
+profileNameField.value = profileName.textContent;
+profileDescriptionField.value = profileDescription.textContent;
+
+/* edit profile information */
+// Находим форму в DOM
+const formElement = editProfileForm;// Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+const nameInput = profileNameField;// Воспользуйтесь инструментом .querySelector()
+const jobInput = profileDescriptionField;// Воспользуйтесь инструментом .querySelector()
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function profileEditFormSubmit(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+  // Получите значение полей jobInput и nameInput из свойства value
+  let enteredName = nameInput.value;
+  let enteredJob = jobInput.value;
+
+  // Выберите элементы, куда должны быть вставлены значения полей
+
+  // Вставьте новые значения с помощью textContent
+  profileName.textContent = enteredName;
+  profileDescription.textContent = enteredJob;
+
+  closePopup();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', profileEditFormSubmit); 
+
+/* creating a new card and adding it to front */
+const creatingNewCardForm = document.forms['new-place'];
+
+const newPlaceNameField = creatingNewCardForm['place-name'];
+const newPlaceImageLinkField = creatingNewCardForm['link'];
+
+function creatingNewCardFormSubmit(evt) {
+  evt.preventDefault();
+
+  let enteredPlaceName = newPlaceNameField.value;
+  let enteredImageLink = newPlaceImageLinkField.value;
+
+  cardsList.prepend(createCard(enteredImageLink, enteredPlaceName, deleteCard));
+
+  closePopup();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+creatingNewCardForm.addEventListener('submit', creatingNewCardFormSubmit);
